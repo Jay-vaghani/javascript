8ab4e1297b1e9bb2b898dcfc61be6061402c0605
Jay-vaghani <89519011+Jay-vaghani@users.noreply.github.com>
2023-03-21 10:29:12 +0530

    javascript numbers

---
+++ app.js
@@ -0,0 +1,70 @@
+// var a = 5;
+// var b = "abc";
+
+// console.log(a);
+// console.log(b);
+
+// (1). String
+// (2). Number
+
+// (4). Boolean
+// (5). Undefined
+// (6). Null
+
+// (7). Arr
+// (8). Object
+
+// ******************************** JavaScript Number ********************************
+// var a = 7;
+// var b = 5;
+
+// var c = a + b;
+
+// var d = a - b;
+// var e = a / b;
+
+// var f = 1;
+// f++;
+// f++;
+// f++;
+// f++;
+// f--;
+// f--;
+// f--;
+// f--;
+
+// var g = 10;
+
+// g += 10; // g = g + 10
+// g -= 10; // g = g + 10
+// g *= 10; // g = g + 10
+// g /= 10; // g = g + 10
+
+// var h = 2 ** 3; // h = 2 * 2 * 2;
+
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+
+// var i = Math.floor(3.9)
+// var i = Math.ceil(3.1)
+// var i = Math.round(3.4)
+
+// console.log(i);
+
+// var j = Math.random()
+
+// var player1 = Math.floor(Math.random() * 6) + 1
+// var player2 = Math.floor(Math.random() * 6) + 1
+
+// console.log(player1);
+// console.log(player2);
+
+// var k = Number("10");
+
+// var l = 10 + k;
+
+// console.log(l);
