e9484684ec151f0e0b9f509517d5d4823b0aaeed
Jay-vaghani <89519011+Jay-vaghani@users.noreply.github.com>
2023-03-25 10:03:21 +0530

    Javascript if else and ternary operator

--- app.js
+++ app.js
@@ -1,154 +1,18 @@
-// var a = 5;
-// var b = "abc";
-
-// console.log(a);
-// console.log(b);
-
-// (1). String
-// (2). Number
-
-// (4). Boolean
-// (5). Undefined
-// (6). Null
-
-// (7). Arr
-// (8). Object
-
-// ******************************** JavaScript if else ********************************
-
 // var v1 = 5;
-// var v2 = 7;
-
-// if (v1 == v2) {
-//   console.log("ok");
-// } else {
-//   console.log("not ok");
-// }
-
+// var v2 = 5;
 // var v3 = 10;
 // var v4 = 20;
 
-// if (v3 != v4) {
-//   console.log("ok");
-// } else {
-//   console.log("not ok");
-// }
+// var condition = v1 === v2 ? console.log("ok") : console.log("not ok");
 
-// var v5 = "abc";
-// var v6 = "abcd";
+// var condition = v1 === v2 ? v3 !== v4 ? console.log("inside ok") : console.log("inside not ok") : console.log("not ok")
 
-// if (v5 == v6) {
-//   console.log("ok");
-// } else {
-//   console.log("not ok");
-// }
 
-// var v7 = 10;
-// var v8 = "10";
+// ************************ Javascript if else Falsy Value ************************
 
-// if (v7 == v8) {
-//   console.log("ok");
-// } else {
-//   console.log("not ok");
-// }
-
-// var v9 = 10;
-// var v10 = "10";
-
-// if (v9 !== v10) {
-//   console.log("ok");
-// } else {
-//   console.log("not ok");
-// }
-
-// var v11 = 10;
-// var v12 = 20;
-
-// if (v11 < v12) {
-//   console.log("ok");
-// } else {
-//   console.log("not ok");
-// }
-
-// var v11 = 10;
-// var v12 = 10;
-
-// if (v11 <= v12) {
-//   console.log("ok");
-// } else {
-//   console.log("not ok");
-// }
-
-// var v13 = 10;
-// var v14 = 5000;
-
-// if (v13 == 10) {
-//   if (v14 === 500) {
-//     console.log("ok");
-//   } else {
-//     console.log("not ok");
-//   }
-// } else {
-//   console.log("not ok");
-// }
-
-// var v15 = 10;
-// var v16 = 5000;
-
-// if (v15 == 10) {
-//   if (v16 !== 500) {
-//     console.log("ok");
-//   } else {
-//     console.log("not ok");
-//   }
-// } else {
-//   console.log("not ok");
-// }
-
-// var age = 18;
-
-// if (age < 16) {
-//   console.log("Drink Milk");
-// }
-
-// if (age >= 16) {
-//   console.log("Ride Activa");
-// }
-
-// if (age >= 18) {
-//   console.log("Ride Everything");
-// }
-
-// var v17 = 20
-// var v18 = 30
-// var v19 = 40
-
-// if (v17 === 20 || v18 === 31 || v19 === 41) {
-//     console.log("ok");
-// } else{
-//     console.log("not ok");
-// }
-
-// var v20 = 20;
-// var v21 = 30;
-// var v22 = 40;
-
-// if (v20 === 20 && v21 === 30 && v22 === 41) {
-//   console.log("ok");
-// } else {
-//   console.log("not ok");
-// }
-
-// var age2 = 18;
-
-// if (age2 < 16) {
-//   console.log("Drink Milk");
-// }
-
-//  else if (age2 >= 16 && age2 <= 17) {
-//   console.log("Ride Activa");
-// }
-
-// else if (age2 >= 18) {
-//   console.log("Ride Everything");
-// }
+// (1) null
+// (2) undefined
+// (3) false
+// (4) NaN
+// (5) 0
+// (6) ""
