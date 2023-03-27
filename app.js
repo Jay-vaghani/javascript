8c71c26c3a26dbda1047966128870043880f4304
Jay-vaghani <89519011+Jay-vaghani@users.noreply.github.com>
2023-03-27 09:58:53 +0530

    Javascript function

--- app.js
+++ app.js
@@ -1,31 +1,120 @@
-// ************************************* JavaScript Switch Statement *************************************
+// ************************************ JavaScript Function ************************************
 
-let day = "tus";
+// let fun = function () {
+//   let v1 = 5;
+//   let v2 = 10;
 
-switch (day) {
-  case "mon":
-    console.log("mon");
-    break;
-  case "tus":
-    console.log("tus");
-    break;
-  case "wen":
-    console.log("wen");
-    break;
-  case "thu":
-    console.log("thu");
-    break;
-  case "fri":
-    console.log("fri");
-    break;
-  case "sat":
-    console.log("sat");
-    break;
-  case "sun":
-    console.log("sun");
-    break;
+//   let v3 = v1 + v2;
 
-  default:
-    console.log("not ok");
-    break;
-}
+//   console.log(v3);
+// };
+
+// fun();
+
+// let fun2 = function(v1 = 10, v2 = 20){
+//     console.log(v1);
+//     console.log(v2);
+// }
+
+// fun2()
+
+// let fun3 = function(v1, v2 ){
+//     console.log(v1);
+//     console.log(v2);
+// }
+
+// fun3(10, 20)
+
+// let fun4 = function(v1 = 30, v2 = 40 ){
+//     console.log(v1);
+//     console.log(v2);
+// }
+
+// fun4(10, 20)
+
+// let fun5 = function(v1 = 30, v2 = 40 ){
+//     console.log(v1);
+//     console.log(v2);
+// }
+
+// fun5(10)
+
+// function fun6 (){
+//     console.log("ok");
+// }
+
+// fun6()
+
+// fun7();
+
+// function fun7() {
+//   console.log("ok");
+// }
+
+// fun8()
+
+// let fun8 = function () {
+//   console.log("ok");
+// };
+
+// ****************** JavaScript Arrow Function ******************
+
+// let fun9 = () => {
+//     console.log("ok");
+// }
+
+// fun9()
+
+// let fun10 = (v1, v2) => {
+//     console.log(v1);
+//     console.log(v2);
+// }
+
+// fun10(20, 30)
+
+// fun11()
+
+// let fun11 = () => {
+//     console.log("ok");
+// }
+
+// ****************** JavaScript Call Back Function ******************
+
+
+// let fun12 = () => {
+//     console.log("ok");
+// }
+
+// let callBackFun = (fun) => {
+//     fun()
+// }
+
+// callBackFun(fun12)
+
+
+// let fun13 = (v1 , v2) => {
+//     console.log(v1);
+//     console.log(v2);
+// }
+
+// let callBackFun2 = (fun) => {
+//     fun(30, 40)
+// }
+
+// callBackFun2(fun13)
+
+
+// let add = (v1, v2) => {
+//     console.log(v1 + v2);
+// }
+
+// let multiply = (v1, v2) => {
+//     console.log(v1 * v2);
+// }
+
+// let calculate = (val1, val2, fun) => {
+//     fun(val1, val2)
+// }
+
+// calculate(20, 30, add)
+// calculate(2, 3, multiply)
