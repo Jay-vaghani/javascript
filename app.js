08ff2e40a6f088a2d778b13e792dae5bfe0328b2
Jay-vaghani <89519011+Jay-vaghani@users.noreply.github.com>
2023-03-30 09:43:40 +0530

    array part 2

--- app.js
+++ app.js
@@ -1,73 +1,25 @@
-// ************************************ JavaScript Array ************************************
+// ************************************ JavaScript Arrays ************************************
 
-// let v1 = "def";
+// let arr1 = [40, 100, 1, 5, 25, 10];
 
-// let arr1 = [1, 2, 3, "abc", v1, true, false];
 
-// console.log(arr1.length);
-// console.log(arr1[0]);
-// console.log(arr1[1]);
-// console.log(arr1[2]);
-// console.log(arr1[3]);
-// console.log(arr1[4]);
-// console.log(arr1[5]);
-// console.log(arr1[6]);
+// let compere = (a, b) => {
+//     return a - b
+// }
 
-// let v2 = arr1[4];
+// console.log(arr1);
 
-// console.log(v2);
 
-// let arr2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+// arr1.sort(compere)
+
+// console.log(arr1);
+
+
+// const arr2 = [40, 100, 1, 5, 25, 10];
 
 // console.log(arr2);
 
-// let arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// console.log(arr3[arr3.length - 1]);
+// arr2[0] = 60
 
-let arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-let str1 = arr4.toString();
-
-// console.log(str1);
-
-let str2 = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-
-let arr5 = str2.split(",");
-
-// console.log(arr5);
-
-let str3 = arr5.join();
-// console.log(str3);
-
-let str4 = arr5.join("|");
-// console.log(str4);
-
-let str5 = arr5.join(" ");
-
-// console.log(str5);
-
-let arr6 =  ["Banana", "Orange", "Apple", "Mango"];
-
-let res1 = arr6.pop()
-
-// console.log(res1);
-// console.log(arr6);
-
-let res2 = arr6.push("chiku")
-
-// console.log(res2);
-// console.log(arr6);
-
-let res3 = arr6.shift()
-
-// console.log(res3);
-// console.log(arr6);
-
-let res4 = arr6.unshift("kiwi")
-
-// console.log(res4);
-// console.log(arr6);
-
-
-
+// console.log(arr2);
