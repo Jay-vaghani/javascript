4f4b911e4431254e6595d4c611de7714c8a02638
Jay-vaghani <89519011+Jay-vaghani@users.noreply.github.com>
2023-03-31 10:47:52 +0530

    loop part 2

--- app.js
+++ app.js
@@ -1,33 +1,58 @@
-// ************************************ JavaScript Loops ************************************
+// ************************************ Array Iteration  ************************************
 
-let v2 = 25;
-let v3 = 50;
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let obj = {
-  name: "name1",
-  age: 50,
-  job: "Good Job",
-  v1: true,
-  fun1: function (fname, lname) {
-    console.log(`${fname} ${lname} `);
-  },
-  nestedObj: {
-    name: "John",
-    lastName: "Doe",
-    age: 50,
-    eyeColor: "blue",
-  },
-  arr1: [1, 2, 3, 4, 5, 6, 7, 8],
-  val: v2,
-  v3,
+// for (i = 0; i < arr.length; i++) {
+//   console.log(arr[i], "===", i);
+// }
 
-  // fun2: () => {
-  //   console.log(this.name); //Will Give Empty Result
-  // },
+// let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  // fun2: function () {
-  //   console.log(this.name);
-  // },
-};
+// for (i = 0; i < arr2.length; i++) {
+//   console.log(arr2[i], "===", i);
+// }
 
-console.log(obj.nestedObj.name);
+// let fun1 = () => {
+
+//   for (i = 0; i < arr2.length; i++) {
+//     console.log(arr2[i], "===", i);
+//   }
+
+// }
+
+// fun1()
+
+// let mul = (num) => {
+//   for (i = 1; i <= 10; i++) {
+//     console.log(`${num} * ${i} = ${num * i}`);
+//   }
+// };
+
+// mul(5)
+
+// let obj = {
+//   type: "Fiat",
+//   model: "500",
+//   color: "white",
+// };
+
+// console.log(obj.type);
+// console.log(obj["type"]);
+
+// for (let i in obj) {
+//   console.log(obj[i]);
+// }
+
+// let arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+// for (let i of arr3) {
+//   console.log(arr3[i], "===", i);
+// }
+
+// let fun = (element) => {
+//   console.log(element);
+// };
+
+// arr3.forEach(fun);
+// arr4.forEach(fun);
